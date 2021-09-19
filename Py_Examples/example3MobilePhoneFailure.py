6bc97f5f7223c29c2cc1e7d64556d91360aeef99
Abie Marshall <abiemarshall94@gmail.com>
2021-09-19 11:32:40 +0100

    Added cumulative intervals

--- Py_Examples/example3MobilePhoneFailure.py
+++ Py_Examples/example3MobilePhoneFailure.py
@@ -23,9 +23,15 @@ prior = BetaHyperParameters(1.6, 0.178)
 sampling = BinomialSampling(200, 172)
 posterior = BetaHyperParameters(prior.alpha + sampling.y, prior.beta + sampling.n - sampling.y)
 
-prior_pdf = beta.pdf(xseq, prior.alpha, prior.beta)
-posterior_pdf = beta.pdf(xseq, posterior.alpha, posterior.beta)
+prior_beta = beta(prior.alpha, prior.beta)
+prior_pdf = prior_beta.pdf(xseq)
+
+posterior_beta = beta(posterior.alpha, posterior.beta)
+posterior_pdf = posterior_beta.pdf(xseq)
 
 plt.plot(xseq, prior_pdf)
 plt.plot(xseq, posterior_pdf)
 plt.show()
+
+print("Chance of being 90% or above: {}".format(1 - posterior_beta.cdf(0.9)))
+print("Chance of being 95% or above: {}".format(1 - posterior_beta.cdf(0.95)))
